1582651482b3860edc3de5759cfcdfde9848b12c
Eric Rovtar <eric.rovtar@me.com>
2017-01-18 14:48:39 -0500

    Add JS-Cookie

--- src/client.js
+++ src/client.js
@@ -6,9 +6,9 @@ import _string from './includes/string.js';
 import _json from './includes/json.js';
 import _fetch from './includes/polyfills/fetch.js';
 import _scroll from './includes/third-party/scroll.min';
+import _cookies from './includes/third-party/js.cookie';
 
 import Page from './components/page/page';
-
 
 ReactDOM.render(
     <Page />,
