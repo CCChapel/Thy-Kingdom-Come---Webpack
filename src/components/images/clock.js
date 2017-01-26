a5908d39d44ec35722eaa27b3e78d5ccf062e47c
Eric Rovtar <eric.rovtar@me.com>
2017-01-26 09:22:33 -0500

    Replace var with let / const for ES6

--- src/components/images/clock.js
+++ src/components/images/clock.js
@@ -19,51 +19,51 @@ export default class Clock extends React.Component {
     
     render() {
         //Allow classes to be passed in to Component
-        var className = "clock ";
+        let className = "clock ";
         if (this.props.className !== undefined) {
             className += this.props.className;
         }
 
         //Setup Height & Width
-        var height = 400;
+        let height = 400;
         if (this.props.height !== undefined) {
             height = this.props.height;
         }
 
-        var width = 400;
+        let width = 400;
         if (this.props.width !== undefined) {
             width = this.props.width;
         }
 
         //Setup image values
-        var cx = width / 2;
-        var cy = height / 2;
-        var r = Math.min(cx, cy) * .8;
-        var origin = {
+        const cx = width / 2;
+        const cy = height / 2;
+        const r = Math.min(cx, cy) * .8;
+        const origin = {
             x: cx,
             y: cy
         };
 
         //Calculate Clock Positions
-        var date = new Date();
-        var second = date.getSeconds();
-        var minute = date.getMinutes() + second / 60;
-        var hour = (date.getHours() % 12) + minute / 60;
+        const date = new Date();
+        let second = date.getSeconds();
+        let minute = date.getMinutes() + second / 60;
+        let hour = (date.getHours() % 12) + minute / 60;
 
         //Calculate Hour Hand
-        var hourA = Math.radians(hour * 30 - 90);
-        var hourPoint = Math.pointOnCircle(cx, cy, r * .6, hourA);
-        var hourPoints = this.formatPoints(origin, hourPoint); 
+        let hourA = Math.radians(hour * 30 - 90);
+        let hourPoint = Math.pointOnCircle(cx, cy, r * .6, hourA);
+        let hourPoints = this.formatPoints(origin, hourPoint); 
 
         //Calculate Minutes Hand
-        var minuteA = Math.radians(minute * 6 - 90);
-        var minutePoint = Math.pointOnCircle(cx, cy, r * .8, minuteA);
-        var minutePoints = this.formatPoints(origin, minutePoint);
+        let minuteA = Math.radians(minute * 6 - 90);
+        let minutePoint = Math.pointOnCircle(cx, cy, r * .8, minuteA);
+        let minutePoints = this.formatPoints(origin, minutePoint);
 
         //Calculate Seconds Hand
-        var secondA = Math.radians(second * 6 - 90);
-        var secondPoint = Math.pointOnCircle(cx, cy, r * .85, secondA);
-        var secondPoints = this.formatPoints(origin, secondPoint);
+        let secondA = Math.radians(second * 6 - 90);
+        let secondPoint = Math.pointOnCircle(cx, cy, r * .85, secondA);
+        let secondPoints = this.formatPoints(origin, secondPoint);
 
         return (
             <svg className={className} width={width} height={height}>
